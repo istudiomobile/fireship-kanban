236a9e94ad74448f40dc46c8953f67bd4e930179
NgularBoy <alfredo.ramirez@scorpion.co>
2020-07-22 10:00:39 -0500

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAAA1qICmi24omXspupOGnQ-2rpi0qkqbU",
+    authDomain: "kanban-plus.firebaseapp.com",
+    databaseURL: "https://kanban-plus.firebaseio.com",
+    projectId: "kanban-plus",
+    storageBucket: "kanban-plus.appspot.com",
+    messagingSenderId: "1044557131342",
+    appId: "1:1044557131342:web:18b6a11868430b23755d1f"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAAA1qICmi24omXspupOGnQ-2rpi0qkqbU",
+    authDomain: "kanban-plus.firebaseapp.com",
+    databaseURL: "https://kanban-plus.firebaseio.com",
+    projectId: "kanban-plus",
+    storageBucket: "kanban-plus.appspot.com",
+    messagingSenderId: "1044557131342",
+    appId: "1:1044557131342:web:18b6a11868430b23755d1f"
+  }
 };
 
 /*
